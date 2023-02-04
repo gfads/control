61233ec355be9a1aec028e515dab4a4ec55cca40
Nelson <nsrosa@gmail.com>
2023-02-04 14:18:49 +0100

    Update of MQTT Client

--- src/app/mqtt/main/main.go
+++ src/app/mqtt/main/main.go
@@ -8,21 +8,21 @@ import (
 
 func main() {
 
-	// instantiate a controller
+	// On Off controllers
+	//c := controllers.NewController(shared.BASIC_ONOFF,1.0, 1200.0)
+	//c := controllers.NewController(shared.HYSTERESIS_ONOFF,  1.0, 1200.0, 2.7/16.0)
+	//c := controllers.NewController(shared.DEAD_ZONE_ONOFF,  1.0, 1200.0, 2.7/16.0)
 
-	//c := controller.NewController(shared.PID, shared.DEAD_ZONE_PID, -9600, 0.01, 1.0, 2.7/10)
-	//c := controller.NewController(shared.PID, shared.INCREMENTAL_FORM_PID, -9600, 0.01, 1.0)
-	//c := controller.NewController(shared.PID, shared.ERROR_SQUARE_PID, -9600, 0.01, 1.0)
-	//c := controller.NewController(shared.PID, shared.SMOOTHING_PID, -9600, 0.01, 1.0)
-	//c := controller.NewController(shared.PID, shared.BASIC_PID, -9600, 0.0, 0.0) // P Controller
-	//c := controller.NewController(shared.PID, shared.BASIC_PID, -9600, 0.01, 0.0) // PI Controller
-	//c := controller.NewController(shared.PID, shared.BASIC_PID, -9600, 0.01, 1.0) // PID Controller
-	//c := controller.NewController(shared.ONOFF, "", 1.0, 1200.0) // OnOff controller
-	//c := controller.NewController(shared.HYSTERESIS_ONOFF, "", 1.0, 1200.0, 2.7/16.0) // 25% of 2.7 Hysteresis zone controller
-	//c := controller.NewController(shared.DEAD_ZONE_ONOFF, "", 1.0, 1200.0, 2.7/16.0) // Dead zone controller
-	//c := controller.NewController(shared.GAIN_SCHEDULING, shared.BASIC_PID, 0.0, 0.0, 0.0) // Gain Scheduling PID adaptive control
+	// PID controllers
+	//c := controllers.NewController(shared.BASIC_PID, -9600, 0.0, 0.0) // P Controller
+	//c := controllers.NewController(shared.BASIC_PID, -9600, 0.01, 0.0) // PI Controller
+	//c := controllers.NewController(shared.BASIC_PID, -9600, 0.01, 1.0) // PID Controllerc = controllers.NewController(shared.DEAD_ZONE_PID, -9600, 0.01, 1.0, 2.7/10)
+	//c := controllers.NewController(shared.INCREMENTAL_FORM_PID, -9600, 0.01, 1.0)
+	//c := controllers.NewController(shared.ERROR_SQUARE_PID, -9600, 0.01, 1.0)
+	//c := controllers.NewController(shared.SMOOTHING_PID, -9600, 0.01, 1.0)
 
-	c := controllers.NewController(shared.BASIC_ONOFF, 1.0, 1200.0)
+	// Gain Scheduling controller
+	c := controllers.NewController(shared.GAIN_SCHEDULING, 0.0, 0.0, 0.0)
 
 	// create client and use the controller instance
 	client := myclient.NewMyMQTTClient(shared.BROKER_ADDRESS, shared.BROKER_PORT, c)
